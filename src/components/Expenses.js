267c4385a57b7d611730f6e51044e00549d88afa
Dilenio Enderle <dilenio@gmail.com>
2020-11-02 14:17:21 -0300

    Eslinter corrections 3

--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 import { fetchCurrency, addExpense } from '../actions';
 import '../css/expenses.css';
 
